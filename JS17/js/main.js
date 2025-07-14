863f403953d06fc577aeb46fa19acbb391fd2ac7
your name <your email>
2025-07-14 17:11:46 +0800

    Classes

---
+++ JS17/js/main.js
@@ -0,0 +1,17 @@
+// Javascript Classes
+
+class Pizza {
+  constructor(pizzaType) {
+    this.type = pizzaType;
+    this.size = "medium";
+    this.crust = "original";
+  }
+  bake() {
+    console.log(
+      `Baking a ${this.size} ${this.crust} ${this.type} crust pizza.`
+    );
+  }
+}
+
+const myPizza = new Pizza("pepperoni");
+myPizza.bake();
