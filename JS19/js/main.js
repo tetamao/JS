18f1219e9b2499d652b582ae6960ca03a996a6c3
your name <your email>
2025-07-17 14:09:13 +0800

    add More Details of Error Handling

--- JS19/js/main.js
+++ JS19/js/main.js
@@ -1,1 +1,42 @@
 // JavaScript Errors and Error Handling
+"use strict";
+// const variable = "Dave";
+// console.log(variable);
+
+// Object.create();
+
+// const name = "Dave";
+// name = "Joe";
+
+// const makeError = () => {
+//   try {
+//     const name = "Dave";
+//     name = "Joe";
+//   } catch (err) {
+//     console.warn(err);
+//     console.log(err);
+//     console.table(err);
+//     console.error(err.name);
+//     console.error(err.stack);
+//     console.error(err.message);
+//   }
+// };
+// makeError();
+
+const makeError = () => {
+  let i = 1;
+  while (i <= 5) {
+    try {
+      if (i % 2 !== 0) {
+        throw new Error("Odd number!");
+      }
+      console.log("Even number!");
+    } catch (err) {
+      console.error(err.stack);
+    } finally {
+      console.log("...finally");
+      i++;
+    }
+  }
+};
+makeError();
