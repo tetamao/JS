138a3adf24c344252ddb2ca9fe78042e53d609bd
your name <your email>
2025-07-22 17:00:18 +0800

    add New Files

--- JS21/js/main.js
+++ JS21/js/main.js
@@ -1,4 +1,13 @@
 // JavaScript Event Listeners
 
 const view = document.querySelector("#view2");
-console.log(view);
+const div = view.querySelector("div");
+const h2 = div.querySelector("h2");
+
+//syntax: addEventListener("event, function, useCapture)
+
+const doSomething = () => {
+  alert("doint something");
+};
+
+h2.addEventListener("click", doSomething, false);
